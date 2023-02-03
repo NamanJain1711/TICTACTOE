dd1d54106715c0a041bcd2ff8b517d846cdbb005
NAMAN JAIN <nj44979@gmail.com>
2023-02-04 01:19:48 +0530

    4-2-23

---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Square = ({value , onClick,isWinningSquare}) => {
+    // console.log(props);
+  return (
+    <button type="button" onClick={onClick} className = {`square ${isWinningSquare ? 'winning' : ''} ${value === 'X' ? 'text-green' : 'text-orange'}`}>
+        {value}
+    </button>
+  )
+}
+
+export default Square
